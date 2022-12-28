46a432a8cc71b3e578109a571d11bd14bf01e90c
jyoon <wldbszp@gmail.com>
2022-12-28 20:15:48 +0900

    Env: next.js 13버전 업데이트로 인한 변경

--- components/Layout/header.tsx
+++ components/Layout/header.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 import { CMS_NAME } from "lib/constants";
 import styles from "components/Layout/layout.module.scss";
